432cdfee9cb85b87d89be107e0c1a3bae18b1bc3
VictorLopesz <euvictor_h@hotmail.com>
2023-10-19 16:38:56 -0300

    trabalho finalizado

--- quiz/script.js
+++ quiz/script.js
@@ -6,12 +6,21 @@ const content = document.querySelector(".content");
 const contentFinish = document.querySelector(".finish");
 const btnRestart = document.querySelector(".finish button");
 
-import questions from "./questoes.js"
+import questions from "./questoes.js";
 
 let currentIndex = 0;
 let questionsCorrect = 0;
 
 btnRestart.onclick = () => {
+    // Limpe o conteúdo do elemento 'contentFinish'
+    contentFinish.innerHTML = "";
+
+    // Remova a classe 'hidden' da imagem 'covid-questions'
+    document.querySelector(".covid-questions").classList.remove("hidden");
+
+    // Remova a imagem de resultado anterior
+    document.querySelector(".result-image").src = "";
+
     content.style.display = "flex";
     contentFinish.style.display = "none";
 
@@ -20,41 +29,65 @@ btnRestart.onclick = () => {
     loadQuestion();
 };
 
-    function nextQuestion(e) {
-        const selectedButton = e.target;
-        const isCorrect = selectedButton.getAttribute("data-correct") === "true";
-    
-        if (isCorrect) {
-            questionsCorrect++;
-        } else {
-            selectedButton.style.backgroundColor = "red";
-            const correctButton = answers.querySelector(`button[data-correct="true"]`);
-            correctButton.style.backgroundColor = "green";
-        }
-    
+function nextQuestion(e) {
+    const selectedButton = e.target;
+    const isCorrect = selectedButton.getAttribute("data-correct") === "true";
+
+    if (isCorrect) {
+        questionsCorrect++;
+        selectedButton.style.backgroundColor = "green"; 
+    } else {
+        selectedButton.style.backgroundColor = "red"; 
+        const correctButton = answers.querySelector(`button[data-correct="true"]`);
+        correctButton.style.backgroundColor = "green";
+    }
+
+    document.querySelectorAll(".answer").forEach((item) => {
+        item.removeEventListener("click", nextQuestion);
+    });
+
+    setTimeout(() => {
         if (currentIndex < questions.length - 1) {
             currentIndex++;
             loadQuestion();
         } else {
             finish();
         }
-    }
-    
+    }, 1500);
+}
 
 function finish() {
     const feedback = document.createElement("div");
     feedback.classList.add("feedback");
+
     if (questionsCorrect === questions.length) {
-        feedback.innerText = `Parabéns, você acertou todas as ${questions.length} questões!`;
+        feedback.innerText = `Parabéns, você acertou todas as ${questions.length} questões. Se proteja sempre e cuide de quem você ama!`;
+        feedback.classList.add("feedback-congrats");
+        document.querySelector(".result-image").src = "../img/congrats.jpg";
+
+    } else if (questionsCorrect >= 7) {
+        feedback.innerText = `Você está por dentro do assunto, acertou ${questionsCorrect} de ${questions.length}. Se proteja sempre e cuide de quem você ama.`;
+        feedback.classList.add("feedback-positive");
+        document.querySelector(".result-image").src = "../img/positive.jpg";
+
+    } else if (questionsCorrect >= 5) {
+        feedback.innerText = `Você acertou apenas ${questionsCorrect} de ${questions.length}. Melhor pesquisar mais sobre o assunto para não dar bobeira.`;
+        feedback.classList.add("feedback-neutral");
+        document.querySelector(".result-image").src = "../img/neutro.jpg";
+
     } else {
-        feedback.innerText = `Poxa, você só acertou ${questionsCorrect} de ${questions.length} questões. Vamos refazer?`;
+        feedback.innerText = `Coronavírus não é brincadeira. Você acertou apenas ${questionsCorrect} de ${questions.length}. Pesquise mais sobre o assunto e se proteja.`;
+        feedback.classList.add("feedback-negative");
+        document.querySelector(".result-image").src = "../img/negative.avif";
+
     }
+
+    document.querySelector(".covid-questions").classList.add("hidden");
 
     content.style.display = "none";
     contentFinish.style.display = "flex";
     contentFinish.appendChild(feedback);
 }
-
 
 function loadQuestion() {
     spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
@@ -73,6 +106,7 @@ function loadQuestion() {
 
         answers.appendChild(div);
     });
+
     document.querySelectorAll(".answer").forEach((item) => {
         item.addEventListener("click", nextQuestion);
     });
